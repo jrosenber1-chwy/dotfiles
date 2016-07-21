730070546144b10f5eb4346a41c3c891128ae06a
Jeff Rosenberg <jeff.rosenberg@olson.com>
2016-07-21 15:35:39 -0500

    Updated PowerShell alias wrappers
    Switched from using Get verb to Use, changed some wrappers to refer to new git aliases, and added a few new aliases

--- Scripts/rosenberg.gitalias.ps1
+++ Scripts/rosenberg.gitalias.ps1
@@ -1,27 +1,59 @@
-#git log
-function Get-GitLog { & git lg $args }
-Set-Alias -Name lg -Value Get-GitLog
-Set-Alias -Name gl -Value Get-GitLog -Force -Option AllScope
+################################################
+# PowerShell wrappers for Git commands / aliases
+#-----------------------------------------------
+# 
+# Where possible, prefer to create aliases in
+# .gitconfig and just wrap in PowerShell
+#
+# This way, can update command behaviors,
+# formatting, etc. across all providers
+# in .gitconfig 
+################################################
 
-function Get-ShortGitLog { & git lg -5 }
-Set-Alias -Name slg -Value Get-ShortGitLog
+#git log
+function Use-GitLg { & git lg $args } #Git alias
+Set-Alias -Name lg -Value Use-GitLg
+Set-Alias -Name gl -Value Use-GitLg -Force -Option AllScope
+
+function Use-GitSlg { & git slg } #Git alias
+Set-Alias -Name slg -Value Use-GitSlg
 
 #git status
-function Get-GS { & git status $args } #Get-GitStatus taken by posh-git
-Set-Alias -Name status -Value Get-GS
-Set-Alias -Name gs -Value Get-GS
+function Use-GitSt { & git st $args } #Git alias
+Set-Alias -Name status -Value Use-GitSt
+Set-Alias -Name gs -Value Use-GitSt
 
 #git add
-function Get-GitAdd { & git add $args }
-Set-Alias -Name add -Value Get-GitAdd
-Set-Alias -Name ga -Value Get-GitAdd
+function Use-GitAdd { & git add $args }
+Set-Alias -Name add -Value Use-GitAdd
+Set-Alias -Name ga -Value Use-GitAdd
 
 #git diff
-function Get-GitDiff { & git diff $args }
-Set-Alias -Name diff -Value Get-GitDiff -Force -Option AllScope
-Set-Alias -Name gd -Value Get-GitDiff
+function Use-GitDiff { & git diff $args }
+Set-Alias -Name diff -Value Use-GitDiff -Force -Option AllScope
+Set-Alias -Name gd -Value Use-GitDiff
 
-#git diff
-function Get-GitCommit { & git commit $args }
-Set-Alias -Name commit -Value Get-GitCommit -Force -Option AllScope
-Set-Alias -Name gc -Value Get-GitCommit -Force -Option AllScope
+#git commit
+function Use-GitCommit { & git commit $args }
+Set-Alias -Name commit -Value Use-GitCommit -Force -Option AllScope
+Set-Alias -Name gc -Value Use-GitCommit -Force -Option AllScope
+
+#git checkout
+function Use-GitChk { & git chk $args } #Git alias
+Set-Alias -Name checkout -Value Use-GitChk -Force -Option AllScope
+Set-Alias -Name chk -Value Use-GitChk -Force -Option AllScope
+Set-Alias -Name gchk -Value Use-GitChk -Force -Option AllScope
+
+function Use-GitDev { & git dev $args } #Git alias
+Set-Alias -Name dev -Value Use-GitDev -Force -Option AllScope
+Set-Alias -Name gcd -Value Use-GitDev -Force -Option AllScope
+Set-Alias -Name gdev -Value Use-GitDev -Force -Option AllScope
+
+#git pull
+function Use-GitPull { & git pull $args }
+Set-Alias -Name pull -Value Use-GitPull -Force -Option AllScope
+Set-Alias -Name gp -Value Use-GitPull -Force -Option AllScope
+
+#git push
+function Use-GitPush { & git push $args }
+Set-Alias -Name push -Value Use-GitPush -Force -Option AllScope
