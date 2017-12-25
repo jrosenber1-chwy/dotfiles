fe3817c2d0bf216c4ddfab419c5bad08e7c0bb7d
Jeff Rosenberg <rosenberg.jeff@gmail.com>
2017-12-25 13:57:10 -0600

    Fix issues on my personal Mac, including changing all shell files to LF line endings

--- Bash/rosenberg.bashrc.sh
+++ Bash/rosenberg.bashrc.sh
@@ -3,11 +3,6 @@
 # Any Bash-specific environment variables here
 
 
-# Source more environment variables from .profile
-if [ -f ~/.profile ]; then
-   source ~/.profile
-fi
-
 # Command prompt
 export PS1='\n\[\e[33m\]\w\[\e[m\]\n\$ '
 export PS2=">>> "
@@ -15,10 +10,10 @@ export PS2=">>> "
 # Git-fu
 GIT_PS1_SHOWUPSTREAM="auto"
 GIT_PS1_SHOWCOLORHINTS="yes"
-source /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh
+source "/Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh"
 export PROMPT_COMMAND='__git_ps1 "\n\[\e[33m\]\w\[\e[m\]" "\n\\\$ ";'
-source /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash
-source ~/Documents/ShellTools/Git/rosenberg.gitalias.sh
+source "/Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash"
+source "$SHELLTOOLS/Git/rosenberg.gitalias.sh"
 
 #
 # Aliases
--- Bash/sample.bashrc.sh
+++ Bash/sample.bashrc.sh
@@ -2,5 +2,10 @@
 # Contains basic preferences and sources rosenberg.bashrc.sh
 # Place this in ~/.bashrc
 
+# Source more environment variables from .profile
+if [ -f ~/.profile ]; then
+   source ~/.profile
+fi
+
 # Shared configuration
 source "$SHELLTOOLS/Bash/rosenberg.bashrc.sh"
