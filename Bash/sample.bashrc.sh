488d81a06f9ed771c3ec2b7e4b1dd9b489a42924
Jeff Rosenberg <jeff.rosenberg@icfolson.com>
2017-12-25 06:22:29 -0600

    Create profile and sample Bash scripts, add SHELLTOOLS environment variable

---
+++ Bash/sample.bashrc.sh
@@ -0,0 +1,6 @@
+# A sample of a .bashrc file to use in setting up a new environment
+# Contains basic preferences and sources rosenberg.bashrc.sh
+# Place this in ~/.bashrc
+
+# Shared configuration
+source "$SHELLTOOLS/Bash/rosenberg.bashrc.sh"
---
+++ Bash/sample.profile.sh
@@ -0,0 +1,9 @@
+# A sample of a .profile file to use in setting up a new environment
+# Contains basic preferences and sources rosenberg.profile.sh
+# Place this in ~/.profile
+
+# Create environment variable pointing to ShellTools
+export SHELLTOOLS="~/Documents/ShellTools"
+
+# Shared configuration
+source "$SHELLTOOLS/Bash/rosenberg.profile.sh"
