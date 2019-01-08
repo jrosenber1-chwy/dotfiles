31f1153a89e668aa794ff360e5bbe6a175b213b8
Jeff Rosenberg <jeff.rosenberg@icfolson.com>
2019-01-08 09:32:37 -0600

    Update sample profile to include PATH and EDITOR environment variables

--- Bash/sample.profile.sh
+++ Bash/sample.profile.sh
@@ -2,8 +2,13 @@
 # Contains basic preferences and sources rosenberg.profile.sh
 # Place this in ~/.profile
 
-# Create environment variable pointing to DOTFILES
-export DOTFILES="~/Documents/dotfiles"
+# Environment variables
+export DOTFILES="~/Documents/dotfiles" # environment variable for my personal dotfiles
+export PATH="$PATH" # try to keep PATH mostly defined in .profile rather than .bashrc
 
 # Shared configuration
 source "$DOTFILES/Bash/rosenberg.profile.sh"
+
+# Use VS Code as the default editor
+# May not always want this, so not placing in rosenberg.profile.sh
+export EDITOR="code --new-window --wait" # Visual studio code -- should already be in path
