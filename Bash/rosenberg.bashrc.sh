67fa61c3fec5189ab12d4f824bdedb0336d7999f
Jeff Rosenberg <jeff.rosenberg@icfolson.com>
2019-01-08 09:31:52 -0600

    Move some aliases from bashrc into profile

--- Bash/rosenberg.bashrc.sh
+++ Bash/rosenberg.bashrc.sh
@@ -20,11 +20,6 @@ source "/Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-comple
 
 # Mac OS CLI
 eval $(thefuck --alias)
-alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
-alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
-alias showfiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
-alias hidefiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
-alias flushcache='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder;'
 
 # Git - reference separate file
 source "$DOTFILES/Git/rosenberg.gitalias.sh"
---
+++ Bash/rosenberg.profile.sh
@@ -0,0 +1,10 @@
+#
+# Aliases
+#
+
+# Mac OS CLI
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+alias showfiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hidefiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+alias flushcache='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder;'
