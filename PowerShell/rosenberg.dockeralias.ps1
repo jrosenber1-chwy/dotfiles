90d133df34c9b248d1fccf1a21e1f31b28b53297
Jeff Rosenberg <jeff.rosenberg@icfolson.com>
2019-06-05 08:59:56 -0500

    Add docker aliases

---
+++ PowerShell/rosenberg.dockeralias.ps1
@@ -0,0 +1,23 @@
+################################################
+# Docker aliases
+################################################
+
+# docker container ls
+function Use-DockerLs { & docker container ls $args } 
+Set-Alias -Name dls -Value Use-DockerLs
+function Use-DockerLsa { & docker container ls -a } 
+Set-Alias -Name dlsa -Value Use-DockerLsa
+
+# docker container rm
+function Use-DockerRm { & docker container rm $args }
+Set-Alias -Name drm -Value Use-DockerRm
+
+# docker container logs
+function Use-DockerContainerLogs { & docker container logs $args }
+Set-Alias -Name dl -Value Use-DockerContainerLogs
+function Use-DockerContainerLogsFollow { & docker container logs -f $args }
+Set-Alias -Name dlf -Value Use-DockerContainerLogsFollow
+
+# docker volume ls
+function Use-DockerVolumeLs { & docker volume ls $args } 
+Set-Alias -Name dvls -Value Use-DockerVolumeLs
