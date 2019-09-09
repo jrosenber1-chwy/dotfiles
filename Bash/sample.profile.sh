d3369e7d1519270e39c7a4067c7338022513226f
Rosenberg, Jeff <Jeff.Rosenberg@icfnext.com>
2019-09-09 12:27:46 -0500

    Modify paths in sample profile

--- Bash/sample.profile.sh
+++ Bash/sample.profile.sh
@@ -3,12 +3,17 @@
 # Place this in ~/.profile
 
 # Environment variables
-export DOTFILES="~/Documents/dotfiles" # environment variable for my personal dotfiles
-export PATH="$PATH" # try to keep PATH mostly defined in .profile rather than .bashrc
+export DOTFILES="~/dotfiles" # environment variable for my personal dotfiles
+
+# On MacOS, instead of environment variables, add paths to /etc/paths.d/personal
+
+# On Linux, make additions to PATH below - try to keep PATH mostly defined in .profile rather than .bashrc
+# export PATH="$PATH"
 
 # Shared configuration
 source "$DOTFILES/Bash/rosenberg.profile.sh"
 
 # Use VS Code as the default editor
 # May not always want this, so not placing in rosenberg.profile.sh
+# Other options include nano, vi, Notepad++, etc.
 export EDITOR="code --new-window --wait" # Visual studio code -- should already be in path
