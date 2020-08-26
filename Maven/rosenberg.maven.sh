8bea15dae6647ad795494eabe7fcb517f6f1a179
Rosenberg, Jeff <jeff.rosenberg@icfnext.com>
2020-08-26 07:52:22 -0500

    Add Maven alias to run tests for a pattern of test files

--- Maven/rosenberg.maven.sh
+++ Maven/rosenberg.maven.sh
@@ -4,9 +4,14 @@
 # Maven aliases
 ########################################
 
+run_tests_for_pattern() {
+  mvn clean test -Dtest="*${1}*" -DfailIfNoTests=false | grep "ERROR"
+}
+
 alias mci='mvn clean install -DskipTests -DskipIntegrationTests'
 alias mcit='mvn clean install'
 alias mcp='mvn clean package -DskipTests'
 alias mcpr='mvn clean process-resources'
 alias mpr='mvn process-resources'
 alias mct='mvn clean test'
+alias mctp='run_tests_for_pattern'
