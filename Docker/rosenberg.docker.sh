5804bca2b57b2de23d1f363e3b7a6c4f47bc3f32
Rosenberg, Jeff <jeff.rosenberg@icfnext.com>
2020-08-26 07:51:49 -0500

    Tweak Docker aliases to remove containers more easily

--- Docker/rosenberg.docker.sh
+++ Docker/rosenberg.docker.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Enabled Docker buildkit
+export DOCKER_BUILDKIT=1
+
 ########################################
 # Docker aliases
 ########################################
@@ -7,6 +10,7 @@
 alias dls='docker container ls'
 alias dlsa='docker container ls -a'
 alias drm='docker container rm'
+alias drmf='docker container rm -f'
 alias dl='docker container logs'
 alias dlf='docker container logs --tail 100 -f'
 alias dvls='docker volume ls'
@@ -17,13 +21,13 @@ alias dvls='docker volume ls'
 
 alias dc='docker-compose'
 alias dcu='docker-compose up -d'
-alias dcd='docker-compose down'
+alias dcd='docker-compose down --remove-orphans'
 alias dcs='docker-compose stop'
 alias dcl='docker-compose logs'
 alias dclf='docker-compose logs --tail 100 --follow'
 alias dcj='docker-compose -f docker-compose.jro.yml'
 alias dcju='docker-compose -f docker-compose.jro.yml up -d'
-alias dcjd='docker-compose -f docker-compose.jro.yml down'
+alias dcjd='docker-compose -f docker-compose.jro.yml down --remove-orphans'
 alias dcjd='docker-compose -f docker-compose.jro.yml stop'
 alias dcjl='docker-compose -f docker-compose.jro.yml logs'
 alias dcjlf='docker-compose -f docker-compose.jro.yml logs --tail 100 --follow'
