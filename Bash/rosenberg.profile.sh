64d73017959a3946bae235f5de9604e4c704d7d5
Rosenberg, Jeff <jeff.rosenberg@icfnext.com>
2021-08-18 11:47:37 -0500

    Remove unused alias for 'thefuck' util

--- Bash/rosenberg.profile.sh
+++ Bash/rosenberg.profile.sh
@@ -22,7 +22,6 @@ alias cleard='dirs -c' # Reset pushd/popd stack
 alias resetd='dirs -c' # Reset pushd/popd stack
 alias returnd='pushd -0 && dirs -c' # Go back to the beginning of the pushd/popd stack
 alias showd='dirs -v' # Show the pushd/popd stack
-eval $(thefuck --alias)
 
 # Mac OS
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
