a1f0e314ef6a54c9696433790a1641e919fe51da
Jeff Rosenberg <jrosenber1@chewy.com>
2021-10-04 14:10:46 -0400

    Add pwsh aliases for directory stacks

--- PowerShell/rosenberg.pwshalias.ps1
+++ PowerShell/rosenberg.pwshalias.ps1
@@ -6,6 +6,52 @@
 # - because I can never remember how to see all of these
 # - https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles
 # - https://devblogs.microsoft.com/scripting/understanding-the-six-powershell-profiles/
+
+##
+# Directory stacks
+##
+
+# Point `cd` at `Push-Location` instead of `Set-Location`
+function Push-JroLocation {
+    param (
+        [Parameter(Position = 0, Mandatory = $true)]
+        [string]$Path,
+        [Parameter(Position = 1)]
+        $StackName = $null
+    )
+    if ($PSBoundParameters.ContainsKey("StackName")) {
+        Push-Location -Path "$Path" -StackName $StackName
+    }
+    else { # Use the current stack
+        Push-Location -Path "$Path"
+    }
+}
+Set-Alias -Name cd -Value Push-JroLocation -Force -Option AllScope # Override the existing alias
+Set-Alias -Name pushd -Value Push-JroLocation -Force -Option AllScope # Override the existing alias
+
+# Override `popd` to accept $StackName as a positional parameter
+function Pop-JroLocation {
+    param (
+        [Parameter(Position = 0)]
+        $StackName = $null
+    )
+    if ($PSBoundParameters.ContainsKey("StackName")) {
+        Pop-Location -StackName $StackName
+    } else { # Use the current stack
+        Pop-Location
+    }
+}
+Set-Alias -Name popd -Value Pop-JroLocation -Force -Option AllScope # Override the existing alias
+
+# Create a `showd` function to show the current directory stack
+function showd ($StackName = $null) { 
+    if ($PSBoundParameters.ContainsKey("StackName")) {
+        Get-Location -Stack -StackName $StackName
+    } else { # Use the current stack
+        Get-Location -Stack
+    }
+}
+
 function Use-Profile { $profile | Select * } 
 Set-Alias -Name profile -Value Use-Profile
 
