42145df0fd04c9f66b9dcb6ceee59ac7e97f043c
Jeff Rosenberg <rosenberg.jeff@gmail.com>
2021-11-20 14:23:17 -0600

    Update AWS config

--- AWS/rosenberg.aws.sh
+++ AWS/rosenberg.aws.sh
@@ -2,6 +2,8 @@
 
 export PATH="/Users/jeff/.ebcli-virtual-env/executables:$PATH"
 export AWS_EB_PROFILE="jeff-dev"
+export AWS_PROFILE="jeff-dev"
+export AWS_CONFIG_FILE="${DOTFILES}/aws/rosenberg.aws.config"
 
 ##
 ## To manage AWS CLI config, see:
